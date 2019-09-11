a3253b922c50a6680bdadec60cb95df5bde77714
Ludovic Levalleux <levalleux_ludo@hotmail.com>
2019-09-11 10:14:53 +0100

    structure OK - login/logout/nav-bar

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,13 +3,27 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RoomsComponent } from './rooms/rooms.component';
+import { RoomDetailComponent } from './room-detail/room-detail.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RoomsComponent,
+    RoomDetailComponent,
+    NavBarComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
+    ReactiveFormsModule, // required to use directives like FormGroup
+    HttpClientModule, // required for injection in authentication.service
     AppRoutingModule
   ],
   providers: [],
---
+++ frontend/src/app/nav-bar/nav-bar.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from '../_services';
+
+@Component({
+  selector: 'app-nav-bar',
+  templateUrl: './nav-bar.component.html',
+  styleUrls: ['./nav-bar.component.scss']
+})
+export class NavBarComponent implements OnInit {
+  currentUser: any;
+
+  constructor(
+    private authenticationService: AuthenticationService
+  ) { 
+    this.authenticationService.currentUser.subscribe(x => this.currentUser = x);
+  }
+
+  ngOnInit() {
+  }
+
+}
