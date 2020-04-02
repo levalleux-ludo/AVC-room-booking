bfeabbf16723a29beed69641df37e45405930f37
Ludovic Levalleux <levalleux_ludo@hotmail.com>
2020-04-02 15:17:02 +0100

    deploy frontend on firebase

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiAuthentication: 'http://3.11.6.164:4000',
-  apiRoomBooking: 'http://3.11.6.164:4000'
+  apiAuthentication: 'https://avc-room-booking-backend.firebaseapp.com',
+  apiRoomBooking: 'https://avc-room-booking-backend.firebaseapp.com'
 };
