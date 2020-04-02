e0e40ac02c9c6b71c9a0124c55f7cd1609f41936
Ludovic Levalleux <levalleux_ludo@hotmail.com>
2020-04-02 14:29:56 +0100

    schedule cleaning jobs once a day at 0:00:00

--- backend/src/_support/task-scheduler.js
+++ backend/src/_support/task-scheduler.js
@@ -4,9 +4,9 @@ const userService = require('../users/user.service');
 const imageService = require('../images/images.service');
 
 
-const schedulePattern = '0 */2 * * * *';
+const schedulePattern = '0 0 0 * * *'; // every day at 0:00:00 = 0 0 0 * * *
 
-// Every hour, look for undefined references in data model
+// Every day, look for undefined references in data model
 // -for instance a user references an organization that is no longer in the database
 var j = schedule.scheduleJob(schedulePattern, roomService.cleanUndefinedRefs);
 var k = schedule.scheduleJob(schedulePattern, userService.cleanUndefinedRefs);
